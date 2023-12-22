c0dd69ed706f1b5c1434f7c2dcc37239d8205017
Luqman Abdurrohman <luqman.abdurrohman@freshconsulting.com>
2023-12-22 11:29:13 -0800

    Initial Commit

---
+++ SwiftUIChatAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftUIChatAnimation
+//
+//  Created by Luqman Abdurrohman on 12/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
